e1635025ab68f8160dbce421d6fb8f811c1fc4f1
liaojunjie <936118815@qq.com>
2020-06-24 12:07:44 +0800

    0.2.0

---
+++ PDUSDK/Classes/PDUSDK.framework/Headers/PDUSDKApi.h
@@ -0,0 +1,35 @@
+//
+//  PDUSDKApi.h
+//  PDUSDKDemo
+//
+//  Created by 俊杰  廖 on 2020/5/15.
+//  Copyright © 2020 俊杰  廖. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Api.h"
+#import "DPApi.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDUSDKApi : NSObject
+
+
++ (instancetype)shared;
+
+/// 启动
+- (void)startUp;
+
+/// 控制是否开启输出日志
+/// @param enable 默认NO
+- (void)setLogEnable:(BOOL)enable;
+
+/// 获取日志输出状态
+- (BOOL)getLogEnable;
+
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
